d045b117bbf6e27a351898fccc529634925a46a9
Qerakl <german111474457@gmail.com>
2024-11-19 19:11:58 +0500

    feat: model Booking  with migration, factory, seeder, and controller

--- database/migrations/2024_11_19_133556_create_bookings_table.php
+++ database/migrations/2024_11_19_133556_create_bookings_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('check_in_date');
+            $table->enum('status', ['confirmed', 'unconfirmed']);
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookings');
     }
 };
