389b82977e2193eff81c6c61a3747930efddb8a8
Qerakl <german111474457@gmail.com>
2024-11-19 20:08:24 +0500

    feat: config jwt
    contoller AuthApiControllet add method: login, me, logout, refresh, respondWithToken

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -24,7 +24,21 @@ class User extends Authenticatable
     ];
 
     public function bookings(){
-
         return $this->hasMany(Booking::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
