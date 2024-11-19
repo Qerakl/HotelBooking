640c5e912360c15d1bce9060ea80fad0986f99a1
Qerakl <german111474457@gmail.com>
2024-11-19 22:56:14 +0500

    feat: test LoginUserApiTest add methods: setUp, test_successful_login, test_login_with_invalid_password, test_login_with_invalid_email

---
+++ tests/Feature/AuthApi/LoginUserApiTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\AuthApi;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginUserApiTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+    }
+    /**
+     * Тест успешного логина.
+     *
+     * @return void
+     */
+    public function test_successful_login(){
+        $response = $this->post('/api/auth/login', [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($this->user);
+        $response->assertJsonStructure([
+            'message',
+        ]);
+    }
+    /**
+     * тест ошибки не правильного пароля
+     *
+     * @return void
+     */
+    public function test_login_with_invalid_password(){
+        $response = $this->post('/api/auth/login', [
+            'email' => $this->user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertStatus(401);
+        $this->assertGuest();
+        $response->assertJsonStructure([
+            'message',
+        ]);
+    }
+
+    /**
+     * тест ошибки не существующего пользователя.
+     *
+     * @return void
+     */
+
+    public function test_login_with_invalid_email(){
+        $response = $this->post('/api/auth/login', [
+            'email' => $this->faker->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertStatus(401);
+        $this->assertGuest();
+        $response->assertJsonStructure([
+            'message',
+        ]);
+    }
+}
