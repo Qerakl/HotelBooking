5094cd68a121bb0d0923670d0229aba9d0ff6514
Qerakl <german111474457@gmail.com>
2024-11-19 20:28:18 +0500

    feat: controller AuthApiController add method register

--- app/Http/Controllers/AuthApiController.php
+++ app/Http/Controllers/AuthApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +16,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', ]]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     /**
@@ -22,6 +24,18 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
+    public function register(RegisterUserRequest $request)
+    {
+        unset($request['password_confirmation']);
+        User::create($request->all());
+
+        if (! $token = auth()->attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(['message' => 'Login successful'])
+            ->cookie('token', $token, 60 * 24, '/', null, true, true); //httpOnly
+    }
 
     public function login()
     {
