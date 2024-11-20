8c2bb6e6ec4e03c6fcdda0ca6304976c4d7dea3c
Qerakl <german111474457@gmail.com>
2024-11-20 20:54:57 +0500

    feat: test RegisterValidation add method test_register_invalid_name_with_not_string_field, test_register_invalid_name_with_max_length, test_register_invalid_email_with_empty_field

---
+++ tests/Feature/AuthApi/Validation/RegisterValidationTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature\AuthApi\Validation;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterValidationTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    /**
+     * Тест валидации на пустой name.
+     *
+     * @return void
+     */
+
+    public function test_register_invalid_name_with_empty_field(){
+        $response = $this->post('/api/auth/register', [
+            'name' => '',
+            'email' => 'test1@test.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('name');
+        $this->assertDatabaseMissing('users', [
+            'email' => 'test1@test.com',
+        ]);
+    }
+
+    /**
+     * Тест валидации на пустой на то что это строка.
+     *
+     * @return void
+     */
+
+    public function test_register_invalid_name_with_not_string_field(){
+        $response = $this->post('/api/auth/register', [
+            'name' => true,
+            'email' => 'test1@test.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('name');
+        $this->assertDatabaseMissing('users', [
+            'email' => 'test1@test.com',
+        ]);
+    }
+    /**
+     * Тест валидации на больше 255 символов name.
+     *
+     * @return void
+     */
+
+    public function test_register_invalid_name_with_max_length(){
+        $name = $this->faker()->text(900);
+        $response = $this->post('/api/auth/register', [
+            'name' => $name,
+            'email' => 'test1@test.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('name');
+        $this->assertDatabaseMissing('users', [
+            'email' => 'test1@test.com',
+        ]);
+    }
+    /**
+     * Тест валидации на пустой email.
+     *
+     * @return void
+     */
+
+    public function test_register_invalid_email_with_empty_field(){
+        $response = $this->post('/api/auth/register', [
+            'name' => 'Mark123',
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+        $this->assertDatabaseMissing('users', [
+            'name' => 'Mark123',
+        ]);
+    }
+
+}
