654bc381e9114ee0293ffdece4bb5e9d7be8cb47
Qerakl <german111474457@gmail.com>
2024-11-20 22:15:12 +0500

    feat: request StoreBookingRequest add validation

--- app/Http/Requests/StoreBookingRequest.php
+++ app/Http/Requests/StoreBookingRequest.php
@@ -9,20 +9,29 @@ class StoreBookingRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return auth()->check();
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'check_in_date' => 'required|date|after_or_equal:today',
+            'check_out_date' => 'required|date|after:check_in_date',
+            'status' => 'required|in:confirmed|unconfirmed',
+        ];
+    }
+
+    /**
+     * Get custom messages for validation errors.
+     */
+    public function messages(): array
+    {
+        return [
+            'check_in_date.required' => 'Дата заезда обязательна для заполнения.',
+            'check_in_date.date' => 'Дата заезда должна быть корректной датой.',
+            'check_in_date.after_or_equal' => 'Дата заезда не может быть раньше сегодняшнего дня.',
+            'check_out_date.required' => 'Дата выезда обязательна для заполнения.',
+            'check_out_date.date' => 'Дата выезда должна быть корректной датой.',
+            'check_out_date.after' => 'Дата выезда должна быть позже даты заезда.',
+            'status.required' => 'Статус бронирования обязателен для заполнения.',
+            'status.in' => 'Статус должен быть либо "confirmed", либо "unconfirmed".',
         ];
     }
 }
