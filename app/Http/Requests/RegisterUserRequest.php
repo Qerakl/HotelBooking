8e57f494bb81a0f89defc3e223ad597c04a8901d
Qerakl <german111474457@gmail.com>
2024-11-20 21:07:43 +0500

    feat: request RegisterUserRequest add validation method max

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -24,8 +24,8 @@ class RegisterUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-            'password_confirmation' => 'required|string|min:6|same:password',
+            'password' => 'required|string|min:6|confirmed|max:255',
+            'password_confirmation' => 'required|string|min:6|same:password|max:255',
         ];
     }
     public function messages(): array
@@ -45,11 +45,13 @@ class RegisterUserRequest extends FormRequest
             'password.string' => 'Пароль должен быть строкой.',
             'password.min' => 'Пароль должен содержать минимум 6 символов.',
             'password.confirmed' => 'Пароль и подтверждение пароля не совпадают.',
+            'password.max' => 'Пароль не должно превышать 255 символов.',
 
             'password_confirmation.required' => 'Поле "Подтверждение пароля" обязательно для заполнения.',
             'password_confirmation.string' => 'Подтверждение пароля должно быть строкой.',
             'password_confirmation.min' => 'Подтверждение пароля должно содержать минимум 6 символов.',
             'password_confirmation.same' => 'Подтверждение пароля должно совпадать с паролем.',
+            'password_confirmation.max' => 'Подтвержение пароля не должно превышать 255 символов.',
         ];
     }
 }
