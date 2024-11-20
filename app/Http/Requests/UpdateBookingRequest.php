695602b23452110dcaadf090143540d26510948b
Qerakl <german111474457@gmail.com>
2024-11-20 22:17:18 +0500

    feat: request UpdateBookingRequest add validation

--- app/Http/Requests/UpdateBookingRequest.php
+++ app/Http/Requests/UpdateBookingRequest.php
@@ -22,7 +22,22 @@ class UpdateBookingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'check_in_date' => 'sometimes|date|after_or_equal:today',
+            'check_out_date' => 'sometimes|date|after:check_in_date',
+            'status' => 'sometimes|in:confirmed,unconfirmed',
+        ];
+    }
+    /**
+     * Get custom messages for validation errors.
+     */
+    public function messages(): array
+    {
+        return [
+            'check_in_date.date' => 'Дата заезда должна быть корректной датой.',
+            'check_in_date.after_or_equal' => 'Дата заезда не может быть раньше сегодняшнего дня.',
+            'check_out_date.date' => 'Дата выезда должна быть корректной датой.',
+            'check_out_date.after' => 'Дата выезда должна быть позже даты заезда.',
+            'status.in' => 'Статус должен быть либо "confirmed", либо "unconfirmed".',
         ];
     }
 }
