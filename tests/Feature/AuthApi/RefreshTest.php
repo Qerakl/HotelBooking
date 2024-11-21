4bae0efb50415fe3f7694a4c144b4505ffa09e1c
Qerakl <german111474457@gmail.com>
2024-11-21 16:29:15 +0500

    feat: add test for refresh method - valid token and unauthorized access

--- tests/Feature/AuthApi/RefreshTest.php
+++ tests/Feature/AuthApi/RefreshTest.php
@@ -2,19 +2,52 @@
 
 namespace Tests\Feature\AuthApi;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class RefreshTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    protected $user;
+
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+    }
+
+    /**
+     * Тест на обновление токена для авторизованного пользователя.
+     */
+    public function test_refresh_with_valid_token()
+    {
+        $token = JWTAuth::fromUser($this->user);
+
+        $response = $this->post('api/auth/refresh', [], [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
+    }
+
+    /**
+     * Тест на попытку обновить токен без авторизации.
+     */
+    public function test_refresh_without_authorization()
+    {
+        $response = $this->post('api/auth/refresh');
+
+        $response->assertStatus(302);
     }
 }
