39dd470fdc1949cbb31090e48696845831b6772a
Qerakl <german111474457@gmail.com>
2024-11-21 16:28:03 +0500

    feat: add test for logout method - authorized and unauthorized access

--- tests/Feature/AuthApi/LogoutTest.php
+++ tests/Feature/AuthApi/LogoutTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature\AuthApi;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LogoutTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    protected $user;
+
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+    }
+
+    /**
+     * Тест на выход из системы для авторизованного пользователя.
+     */
+    public function test_logout_with_authorized_user()
+    {
+        $token = JWTAuth::fromUser($this->user);
+
+        $response = $this->post('api/auth/logout', [], [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
 
         $response->assertStatus(200);
+        $response->assertJson(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Тест на выход из системы без авторизации.
+     */
+    public function test_logout_without_authorization()
+    {
+        $response = $this->post('api/auth/logout');
+
+        $response->assertStatus(302);
     }
 }
