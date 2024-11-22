5a817745b044bbaa9150a33115d44d812fcf3e6a
Qerakl <german111474457@gmail.com>
2024-11-22 13:10:31 +0500

    feat: controller FilterBookingController add paginate

--- app/Http/Controllers/FilterBookingApiController.php
+++ app/Http/Controllers/FilterBookingApiController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class FilterBookingApiController extends Controller
 {
     public function filter(Request $request){
-        $booking = Booking::where('status', $request->input('status'))->get();
+        $booking = Booking::where('status', $request->input('status'))->paginate(10);
         return response()->json($booking);
     }
 }
