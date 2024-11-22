f8eec57def4a495f0f064d453c9c6d42494d9542
Qerakl <german111474457@gmail.com>
2024-11-22 13:28:07 +0500

    feat: request FilterRequest

---
+++ app/Http/Requests/FilterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'status.required' => 'Поле status обязательно для заполнения.',
+        ];
+    }
+}
